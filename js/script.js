439c7778976ad22847aed6613dd99de45efb54d0
karol <kkijowski11@gmail.com>
2023-06-10 21:51:39 +0200

    texts

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 const canvas = document.getElementById('canvas');
-const overlayContent = document.getElementById('overlayContent');
-const closeButton = document.getElementById('closeButton');
+const overlayContent1 = document.getElementById('overlayContent1');
+const overlayContent2 = document.getElementById('overlayContent2');
+const closeButton1 = document.getElementById('closeButton1');
+const closeButton2 = document.getElementById('closeButton2');
 
 const ctx = canvas.getContext('2d');
 
 const spriteWidth = 3393;
 const spriteHeight = 462;
 
-const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_WIDTH = canvas.width = window.innerWidth;
 const CANVAS_HEIGHT = canvas.height = 462;
 
 const backgroundImage = new Image();
 backgroundImage.src = 'assets/formy_arkadowe_punkt_1.png'
+
+const magnifierImage1 = new Image();
+magnifierImage1.src = 'assets/magnifier.png'
+
+const magnifierImage2 = new Image();
+magnifierImage2.src = 'assets/magnifier.png'
 
 // Create a rectangle object
 const rectangle = {
@@ -42,18 +50,36 @@ function drawBackground() {
         0, 0, canvas.width, canvas.height // Destination on the canvas
     );
 
-    ctx.fillStyle = "blue";
-    ctx.fillRect(rectangle.x - cameraX, rectangle.y - cameraY, rectangle.width, rectangle.height);
+    ctx.drawImage(
+        magnifierImage1, // Image to draw
+        0 - cameraX + 2370,
+        0 - cameraY + 200,
+        magnifierImage1.width * 0.5,
+        magnifierImage1.height * 0.5
+    );
+
+    ctx.drawImage(
+        magnifierImage2, // Image to draw
+        0 - cameraX + 2550,
+        0 - cameraY + 260,
+        magnifierImage2.width * 0.5,
+        magnifierImage2.height * 0.5
+    );
 }
 
 // Function to show the overlay content
-function showOverlayContent() {
-    overlayContent.style.display = 'flex';
+function showOverlayContent1() {
+    overlayContent1.style.display = 'flex';
+}
+
+function showOverlayContent2() {
+    overlayContent2.style.display = 'flex';
 }
 
 // Function to close the overlay content
 function closeOverlayContent() {
-    overlayContent.style.display = 'none';
+    overlayContent1.style.display = 'none';
+    overlayContent2.style.display = 'none';
 }
 
 // Event listener for key press
@@ -125,19 +151,46 @@ canvas.addEventListener("click", function (event) {
     const mouseX = event.clientX - rect.left;
     const mouseY = event.clientY - rect.top;
 
-    // Check if the mouse click is within the rectangle
+    // Check if the mouse click is within the first magnifier
     if (
-        mouseX >= rectangle.x &&
-        mouseX <= rectangle.x + rectangle.width &&
-        mouseY >= rectangle.y &&
-        mouseY <= rectangle.y + rectangle.height
+        mouseX >= 0 - cameraX + 2370 &&
+        mouseX <= 0 - cameraX + 2370 + magnifierImage1.width &&
+        mouseY >= 0 - cameraY + 200 &&
+        mouseY <= 0 - cameraY + 200 + magnifierImage1.height
     ) {
-        showOverlayContent();
+        showOverlayContent1();
+        magnifierImage1.src = 'assets/magnifier_active.png'
+        drawBackground()
+
+    }
+
+    // Check if the mouse click is within the second magnifier
+    if (
+        mouseX >= 0 - cameraX + 2550 &&
+        mouseX <= 0 - cameraX + 2550 + magnifierImage2.width &&
+        mouseY >= 0 - cameraY + 260 &&
+        mouseY <= 0 - cameraY + 260 + magnifierImage2.height
+    ) {
+        showOverlayContent2();
+        magnifierImage2.src = 'assets/magnifier_active.png'
+        drawBackground()
     }
 });
 
 // Add event listener to the close button
-closeButton.addEventListener('click', closeOverlayContent);
+closeButton1.addEventListener('click', () => {
+        closeOverlayContent();
+        magnifierImage1.src = 'assets/magnifier.png'
+        magnifierImage2.src = 'assets/magnifier.png'
+        drawBackground()
+});
+
+closeButton2.addEventListener('click', () => {
+    closeOverlayContent();
+    magnifierImage1.src = 'assets/magnifier.png'
+    magnifierImage2.src = 'assets/magnifier.png'
+    drawBackground()
+});
 
 // Initial drawing
 backgroundImage.onload = () => {
